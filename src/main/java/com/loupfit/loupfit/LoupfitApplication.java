cbb0589cb61286f05670a5bb7b73c88449534ec2
Mattheus Fernandes <mattheus.ale@hotmail.com>
2025-06-26 20:04:54 -0300

    iniciado projeto loupfit

---
+++ src/main/java/com/loupfit/loupfit/LoupfitApplication.java
@@ -0,0 +1,13 @@
+package com.loupfit.loupfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoupfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoupfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loupfit/loupfit/LoupfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loupfit.loupfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoupfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
