a27990d65ce0b1529f7261c82232f062532f945b
Mattheus Fernandes <mattheus.ale@hotmail.com>
2025-07-03 18:40:57 -0300

    configurando cors para endpoints

---
+++ src/main/java/com/loupfit/loupfit/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.loupfit.loupfit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/user")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+
+                registry.addMapping("/user/login")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("POST")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
